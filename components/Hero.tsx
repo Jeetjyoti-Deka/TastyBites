713f0f3493e9735d3df8d03826352865bd93c70f
Jeetjyoti Deka <jeetjyoti2020@gmail.com>
2023-10-20 18:03:26 +0530

    Making Hero section responsive

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,30 +2,37 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="h-[90vh] w-full pt-20 bg-secondary">
+    <div className="sm:h-[90vh] w-full pb-20 sm:pb-0 pt-20 mb-40 bg-secondary overflow-hidden">
       <div className="flex w-full h-full max-container px-10">
         <div className="flex-1">
-          <h1 className="text-white text-6xl font-semibold leading-[66px] tracking-wide pt-28">
+          <h1 className="text-white text-3xl sm:text-5xl md:text-6xl font-semibold md:leading-[66px] tracking-wide pt-20 lg:pt-28 z-50">
             Savor the Flavors:
             <br /> Discover Our <br /> Culinary{" "}
             <span className="text-accent font-bold">Delights</span>
           </h1>
         </div>
-        <div className="h-full w-full flex-1 relative">
-          <div className="w-[670px] h-[670px] rounded-full bg-primary absolute -top-52 -right-48">
+        <div className="h-full w-full flex-1 relative hidden sm:block">
+          <div className="hero-img-section">
             <Image
               src="/hero-img-1.png"
               width={430}
               height={430}
               alt="dish-img"
-              className="absolute top-52 -left-40"
+              className="absolute top-52 -left-40 hidden xl:block"
+            />
+            <Image
+              src="/burger-img.png"
+              width={430}
+              height={430}
+              alt="dish-img"
+              className="absolute top-80 -left-40 xl:hidden -rotate-45 rounded-full"
             />
             <Image
               src="/hero-img-2.png"
               width={430}
               height={430}
               alt="dish-img"
-              className="absolute top-96 -right-20"
+              className="absolute top-96 -right-20 hidden xl:block"
             />
           </div>
         </div>
