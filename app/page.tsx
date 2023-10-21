52a2fbfe3b62fa581be2ce1e92c914743761208b
Jeetjyoti Deka <jeetjyoti2020@gmail.com>
2023-10-21 08:19:16 +0530

    Details section setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Details from "@/components/Details";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Details />
     </div>
   );
 }
---
+++ components/Details.tsx
@@ -0,0 +1,43 @@
+const Details = () => {
+  return (
+    <div className="max-container">
+      <div className="flex flex-col gap-4 text-center md:text-left md:flex-row justify-center bg-white px-10 py-10 max-w-[60rem] mx-auto shadow-2xl">
+        <div className="flex-1 md:self-center">
+          <h3 className="mb-2">
+            <span className="font-semibold text-2xl">Melbourne</span>,{" "}
+            <span className="font-medium text-lg text-gray-600">Australia</span>
+          </h3>
+          <p className="md:max-w-sm">
+            263 King Str. 05th Floor, utrol Hosue Building, Velbourne. VIC 3000.
+            Auztralia.
+          </p>
+        </div>
+        <div className="flex-1 flex flex-col gap-4 md:max-w-sm ">
+          <div>
+            <h4 className="font-bold text-lg">Call directly</h4>
+            <p className="font-medium">+0 123 456 789</p>
+          </div>
+          <div>
+            <h4 className="font-semibold text-lg">Lunch Service</h4>
+            <p className="font-medium text-gray-400">
+              Friday, Saturday and Sunday: Bookings from 12 pm to 1:30 pm
+            </p>
+          </div>
+          <div>
+            <h4 className="font-semibold text-lg">Dinner Service</h4>
+            <p className="font-medium text-gray-400">
+              Daily: Bookings from 6 pm to 9:30 pm
+            </p>
+          </div>
+          <div>
+            <h4 className="font-semibold text-lg">Delivery</h4>
+            <p className="font-medium text-gray-400">
+              Daily: from 10 am to 9 pm
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Details;
